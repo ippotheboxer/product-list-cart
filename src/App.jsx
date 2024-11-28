adef2ccf4e1cd294a7b5602fb9aa7fc6bd041a9f
Sophie <sophiesokol123@gmail.com>
2024-11-28 19:50:16 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='h-screen'>
+      <section className="flex items-center justify-start">
+        <h1 className='text-xl'>Desserts</h1>
+        <div className="products"></div>
+        <div className="cart"></div>
+      </section>  
+      </main>
+  );
+}
+
+export default App;
