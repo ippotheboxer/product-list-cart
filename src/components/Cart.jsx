92f12beda9986dc5dbb55f7b2e4ca8a20a34e438
Sophie <sophiesokol123@gmail.com>
2024-11-30 21:30:57 +0000

    Improve css, add additional text to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,24 @@
 import emptyCartIcon from "../assets/images/illustration-empty-cart.svg";
+import iconCarbonNeutral from "../assets/images/icon-carbon-neutral.svg";
 import { useContext } from 'react';
-import { CartContext } from '../context/Cart'
-import PropTypes from 'prop-types'
+import { CartContext } from '../context/Cart';
 
-export default function Cart({showModal, toggle}) {
-  const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } = useContext(CartContext)
+export default function Cart() {
+  const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } = useContext(CartContext);
     return (
         <div className="cart">
-          <h2 className='cartTitle text-2xl font-bold self-start'>Your cart ({cartItems.length})</h2>
-          <div className="flex flex-col gap-4">
+          <h2 className='cartTitle text-2xl font-bold self-start pb-4'>Your cart ({cartItems.length})</h2>
+          <div className="flex flex-col gap-4 w-full">
     {cartItems.map((item) => (
-      <div className="flex justify-between items-center" key={item.id}>
+      <div className="flex justify-between items-center cartItem with-line pb-3" key={item.id}>
         <div className="flex gap-4">
-          <img src={item.image.thumbnail} alt={item.name} className="rounded-md h-24" />
           <div className="flex flex-col">
-            <h1 className="text-lg font-bold">{item.name}</h1>
-            <p className="">{item.price}</p>
+            <h1 className="text-sm font-semibold pb-1">{item.name}</h1>
+            <div className="flex flex-row justify-between">
+              <p className="text-sm font-bold cartTitle">{item.quantity}x</p>
+              <p className="font-light categoryName">@{item.price}</p>
+              <p>${item.price * item.quantity}</p>
+            </div>
           </div>
         </div>
         <div className="flex gap-4">
@@ -27,7 +30,6 @@ export default function Cart({showModal, toggle}) {
           >
             +
           </button>
-          <p>{item.quantity}</p>
           <button
             className="px-4 py-2"
             onClick={() => {
@@ -42,8 +44,15 @@ export default function Cart({showModal, toggle}) {
   </div>
           {
           cartItems.length > 0 ? ( 
-            <div className="flex flex-col justify-between items-center">
-            <h1 className="text-lg font-bold">Total: ${getCartTotal()}</h1>
+            <div className="flex flex-col justify-between items-center self-start w-full">
+              <div className="flex flex-row justify-between items-center w-full pb-4 p-2">
+            <h1 className="text-sm">Order Total</h1>
+            <h2 className="text-xl font-bold">${getCartTotal()}</h2>
+            </div>
+            <div className="flex flex-row carbonNeutral py-3 px-6 rounded-md">
+              <img src={iconCarbonNeutral} />
+              <p className="text-sm">This is a <span className="font-semibold">carbon-neutral</span> delivery </p>
+            </div>
             <button
               className="px-4 py-2"
               onClick={() => {
@@ -55,16 +64,11 @@ export default function Cart({showModal, toggle}) {
           </div>
           ) : ( 
             <>
-            <img src={emptyCartIcon} alt="" />
+            <img src={emptyCartIcon} alt="" className="w-20"/>
           <p className="emptyCartText">Your added items will display here</p>
           </>
           )
           }
         </div>
     );
-}
-
-Cart.propTypes = {
-  showModal: PropTypes.bool,
-  toggle: PropTypes.func
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,7 +9,7 @@ export default function Products() {
 
     return ( 
         <section className="flex justify-start content-center lg:content-start flex-col lg:flex-row self-start"> 
-        <div className="grid lg:grid-cols-3 sm:grid-cols-1 lg:w-7/12 w-full gap-6 pr-8">
+        <div className="grid lg:grid-cols-3 sm:grid-cols-1 lg:w-7/12 xl:w-6/12 w-full gap-6 pr-8 mr-8">
           <h1 className='text-2xl font-bold lg:col-span-3 sm:col-span-1'>Desserts</h1>
         {
         products.map(product => (
@@ -18,7 +18,7 @@ export default function Products() {
                 <img src={product.image.desktop} className='image' />
                 <button onClick={() => addToCart(product)} className='cartButton'><img src={addToCartIcon}/> Add to cart</button>
                 </div>
-                <p className='categoryName pt-2'>{product.category}</p>
+                <p className='categoryName pt-2 text-xs'>{product.category}</p>
                 <p className='font-medium'>{product.name}</p>
                 <p className='price'>${product.price}</p>
                 </div>
